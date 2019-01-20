29802de9698d6f239594479b7970cfda22755bde
Trent1048 <BULTSTRE000@bellinghamschools.org>
2019-01-19 17:27:38 -0800

    fixed some bugs, added NOT_HELD and switchPreset()

--- buttons.py
+++ buttons.py
@@ -1,6 +1,5 @@
 import ctre
 import wpilib
-from enum import Enum
 import time
 
 class Buttons():    
@@ -9,24 +8,36 @@ class Buttons():
         self.SINGLE_CLICK = 1
         self.DOUBLE_CLICK = 2
         self.HELD = 3
+        self.NOT_HELD = 4
         self.joystick = wpilib.Joystick(0)
         self.presets = []
+        self.otherPresets = []
         self.doubleClickDetector = {}
 
     def addPreset(self, button, clickType, function, args):
         self.presets.append((button, clickType, function, args))
+
+    def removePreset(self, button, clickType, function, args):
+        self.presets.remove((button, clickType, function, args))
+
+    def switchPreset(self):
+        self.temp = self.presets
+        self.presets = self.otherPresets
+        self.otherPresets = self.temp
 
     def update(self):
         for button, clickType, function, args in self.presets:
             if self.joystick.getRawButtonPressed(button) and clickType == self.SINGLE_CLICK:
                 function(*args)
             elif self.joystick.getRawButton(button) and clickType == self.HELD:
-                function()
+                function(*args)
+            elif not (self.joystick.getRawButton(button)) and clickType == self.NOT_HELD:
+                function(*args)
             elif clickType == self.DOUBLE_CLICK:
                 if self.doubleClickDetector.get(button) == None:
                     self.doubleClickDetector[button] = time.time
                 if time.time + 0.25 > self.doubleClickDetector[button]:
                     self.doubleClickDetector[button] = time.time
-                    function()  
+                    function(*args)  
                 else:
                     self.doubleClickDetector[button] = time.time             
