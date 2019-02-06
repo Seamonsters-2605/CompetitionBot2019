336e1c4c8b3568dd8b7f189b238b9cabb0d88b06
Jacob van't Hoog <vantjac@gmail.com>
2019-02-06 15:06:51 -0800

    Drive to point action finds shortest distance to angle

--- auto_actions.py
+++ auto_actions.py
@@ -6,8 +6,10 @@ def createWaitAction(time):
     return Action("Wait " + str(time), lambda: sea.wait(int(time * 50)))
 
 def createDriveToPointAction(pathFollower, x, y, angle, time):
+    def generator():
+        newAngle = sea.circleDistance(pathFollower.robotAngle, angle) + pathFollower.robotAngle
+        yield from sea.ensureTrue(
+            pathFollower.driveToPointGenerator(x, y, newAngle, time, math.radians(1)),
+            10)
     return Action("Drive to (%f, %f, %f) for %f" % (x, y, math.degrees(angle), time),
-        lambda: sea.ensureTrue(
-            pathFollower.driveToPointGenerator(x, y, angle, time, math.radians(1)),
-            10),
-        coords=[(x, y)])
+        generator, coords=[(x, y)])
