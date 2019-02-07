6919d685e2732bfd270da86e5c914e4deb593812
Jacob van't Hoog <vantjac@gmail.com>
2019-02-07 15:17:54 -0800

    Drive to point: add position tolerance

--- auto_actions.py
+++ auto_actions.py
@@ -10,8 +10,9 @@ def createDriveToPointAction(pathFollower, x, y, angle, time):
     def generator():
         newAngle = sea.circleDistance(pathFollower.robotAngle, angle) + pathFollower.robotAngle
         yield from sea.ensureTrue(
-            pathFollower.driveToPointGenerator(x, y, newAngle, time, math.radians(1)),
-            10)
+            pathFollower.driveToPointGenerator(x, y, newAngle, time,
+                math.radians(1), 1, math.radians(2)),
+            25)
     return Action("Drive to (%f, %f, %f) for %f" % (x, y, math.degrees(angle), time),
         generator, coords=[(x, y)])
 
