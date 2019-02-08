478418af63297d950418a94edb73ede53ffc843a
Jacob van't Hoog <vantjac@gmail.com>
2019-02-07 22:10:09 -0800

    Clean up grabber

--- grabber.py
+++ grabber.py
@@ -12,6 +12,7 @@ class GrabberArm():
     def __init__(self):
         self.leftSpinner = ctre.WPI_TalonSRX(20)
         self.rightSpinner = ctre.WPI_TalonSRX(21)
+
         self.leftPivot = ctre.WPI_TalonSRX(22)
         self.leftPivot.configSelectedFeedbackSensor(ctre.FeedbackDevice.QuadEncoder, 0, 0)
         self.leftPivotOrigin = self.leftPivot.getSelectedSensorPosition(0)
@@ -19,19 +20,22 @@ class GrabberArm():
         self.rightPivot.configSelectedFeedbackSensor(ctre.FeedbackDevice.QuadEncoder, 0, 0)
         self.rightPivotOrigin = self.rightPivot.getSelectedSensorPosition(0)
         self.rightPivot.setSensorPhase(True)
+
+        # TODO fix names
         self.hatchGrabberOut1 = wpilib.Solenoid(0)
         self.hatchGrabberIn1 = wpilib.Solenoid(1)
         self.hatchGrabberOut2 = wpilib.Solenoid(2)
         self.hatchGrabberIn2 = wpilib.Solenoid(3)
+
         self.compressor = wpilib.Compressor(0)
         self.slideMotor = ctre.WPI_TalonSRX(30)
 
-    #takes in the ball at the speed "speed"
+    #takes in the ball
     def intake(self):
         self.leftSpinner.set(0.35)
         self.rightSpinner.set(-0.35)
 
-    #shoots out the ball at the speed "speed"
+    #shoots out the ball
     def eject(self):
         self.leftSpinner.set(-1)
         self.rightSpinner.set(1)
@@ -40,23 +44,21 @@ class GrabberArm():
         self.leftSpinner.set(0)
         self.rightSpinner.set(0)
 
-    #clamps the grabber arms at the speed "speed"
-    def clawClosed(self):
-        self.leftPivot.set(ctre.ControlMode.Position, self.leftPivotOrigin + CLOSED_POSITION)
-        self.rightPivot.set(ctre.ControlMode.Position, self.rightPivotOrigin - CLOSED_POSITION)
+    def _setClawPosition(self, position):
+        self.leftPivot.set(ctre.ControlMode.Position, self.leftPivotOrigin + position)
+        self.rightPivot.set(ctre.ControlMode.Position, self.rightPivotOrigin - position)
 
-    #releases the grabber arms at the speed "speed"
+    def clawClosed(self):
+        self._setClawPosition(CLOSED_POSITION)
+
     def clawOpen(self):
-        self.leftPivot.set(ctre.ControlMode.Position, self.leftPivotOrigin + OPEN_POSITION)
-        self.rightPivot.set(ctre.ControlMode.Position, self.rightPivotOrigin - OPEN_POSITION)
+        self._setClawPosition(OPEN_POSITION)
 
     def clawBack(self):
-        self.leftPivot.set(ctre.ControlMode.Position, self.leftPivotOrigin + DEFENSE_POSITION)
-        self.rightPivot.set(ctre.ControlMode.Position, self.rightPivotOrigin - DEFENSE_POSITION)
+        self._setClawPosition(DEFENSE_POSITION)
 
     def clawHatch(self):
-        self.leftPivot.set(ctre.ControlMode.Position, self.leftPivotOrigin + HATCH_POSITION)
-        self.rightPivot.set(ctre.ControlMode.Position, self.rightPivotOrigin - HATCH_POSITION)
+        self._setClawPosition(HATCH_POSITION)
 
     #clamps the arms while running the intake wheels to grab the ball
     def grabBall(self):
