bd2226183b64d7e61a48ec12b6a37aef009fbd15
Trent1048 <trentbultsma@gmail.com>
2019-02-19 17:09:12 -0800

    turn everything in path_follower into class PathFinder

--- path_follower.py
+++ path_follower.py
@@ -1,89 +1,95 @@
 import numpy
 from PIL import Image
 
-pic = Image.open("field.png")
-field = numpy.array(pic.getdata(), numpy.uint8).reshape(pic.size[1], pic.size[0], 4)
+#you must download pillow and numpy!!!
 
-#the field is 27 by 54 feet
+class PathFinder:
 
-#TODO: allow user to input picture of feild with all obstacles shaded in that becomes converted to the field list
+    def __init__(self, fieldImage):
+        """
+        :param fieldImage: the file location of an image of the field where all places with obstacles
+        have been filled in black and it has the pixel dimensions 1:1 of the field in feet
+        """
+        pic = Image.open(fieldImage)
+        self.field = numpy.array(pic.getdata(), numpy.uint8).reshape(pic.size[1], pic.size[0], 4)
 
-initPos = [9, 9]#TODO: get location of robot and put here, also shift all values over because this has origin in top left but feild in the middle
-targetPos = [0, 0]#TODO: get the location of cursor and put here
-cost = 1
+    #function to search the path
+    def search(self,field,initPos,targetPos,cost,heuristic):
 
-#has the cost of the robot going away from the goal mapped out so it will only check high cost stuff if there are no other options
-heuristic = [[0 for row in range(len(field[0]))] for col in range(len(field))]
-for i in range(len(field)):    
-    for j in range(len(field[0])):            
-        heuristic[i][j] = abs(i - targetPos[0]) + abs(j - targetPos[1])
-        if field[i][j][0] != 255:
-            heuristic[i][j] = 100 #if there is an obstacle in the spot on the field, adds a very high cost to going over that spot so it will be navigated around
+        #moves we can go for navigating
+        moves = [[-1, 0 ], #up
+                [ 0, -1], #left
+                [ 1, 0 ], #down
+                [ 0, 1 ]] #right
 
-#moves we can go for navigating
-moves = [[-1, 0 ], #up
-         [ 0, -1], #left
-         [ 1, 0 ], #down
-         [ 0, 1 ]] #right
+        refField = [[0 for col in range(len(field[0]))] for row in range(len(field))]#the referrence field
+        refField[initPos[0]][initPos[1]] = 1
+        actField = [[0 for col in range(len(field[0]))] for row in range(len(field))]#the field where the action happens 
 
+        x = initPos[0]
+        y = initPos[1]
+        g = 0
+        f = g + heuristic[initPos[0]][initPos[0]]
+        cell = [[f, g, x, y]]
 
-#function to search the path
-def search(field,initPos,targetPos,cost,heuristic):
+        found = False  #becomes true when robot gets to target position
 
-    refField = [[0 for col in range(len(field[0]))] for row in range(len(field))]#the referrence field
-    refField[initPos[0]][initPos[1]] = 1
-    actField = [[0 for col in range(len(field[0]))] for row in range(len(field))]#the field where the action happens 
+        while not found:
+            cell.sort()#rearrange list to get the move with the least cost
+            cell.reverse()
+            next = cell.pop()
+            x = next[2]
+            y = next[3]
+            g = next[1]
+            f = next[0]
 
-    x = initPos[0]
-    y = initPos[1]
-    g = 0
-    f = g + heuristic[initPos[0]][initPos[0]]
-    cell = [[f, g, x, y]]
+            
+            if x == targetPos[0] and y == targetPos[1]:
+                found = True
+            else:
+                for i in range(len(moves)):#to try out different moves
+                    x2 = x + moves[i][0]
+                    y2 = y + moves[i][1]
+                    if x2 >= 0 and x2 < len(field) and y2 >=0 and y2 < len(field[0]):
+                        if refField[x2][y2] == 0 and field[x2][y2][0] == 255:
+                            g2 = g + cost
+                            f2 = g2 + heuristic[x2][y2]
+                            cell.append([f2, g2, x2, y2])
+                            refField[x2][y2] = 1
+                            actField[x2][y2] = i
+                            
+        revPath = []
+        x = targetPos[0]
+        y = targetPos[1]
+        revPath.append([x, y])#the steps that must be taken to get to the target (in backwards order)
+        while x != initPos[0] or y != initPos[1]:
+            x2 = x - moves[actField[x][y]][0]
+            y2 = y - moves[actField[x][y]][1]
+            x = x2
+            y = y2
+            revPath.append([x, y])
 
-    found = False  #becomes true when robot gets to target position
+        path = []
+        for i in range(len(revPath)):
+            path.append(revPath[len(revPath) - 1 - i])
 
-    while not found:
-        cell.sort()#rearrange list to get the move with the least cost
-        cell.reverse()
-        next = cell.pop()
-        x = next[2]
-        y = next[3]
-        g = next[1]
-        f = next[0]
+        return path
 
-        
-        if x == targetPos[0] and y == targetPos[1]:
-            found = True
-        else:
-            for i in range(len(moves)):#to try out different moves
-                x2 = x + moves[i][0]
-                y2 = y + moves[i][1]
-                if x2 >= 0 and x2 < len(field) and y2 >=0 and y2 < len(field[0]):
-                    if refField[x2][y2] == 0 and field[x2][y2][0] == 255:
-                        g2 = g + cost
-                        f2 = g2 + heuristic[x2][y2]
-                        cell.append([f2, g2, x2, y2])
-                        refField[x2][y2] = 1
-                        actField[x2][y2] = i
-                        
-    revPath = []
-    x = targetPos[0]
-    y = targetPos[1]
-    revPath.append([x, y])#the steps that must be taken to get to the target (in backwards order)
-    while x != initPos[0] or y != initPos[1]:
-        x2 = x - moves[actField[x][y]][0]
-        y2 = y - moves[actField[x][y]][1]
-        x = x2
-        y = y2
-        revPath.append([x, y])
+    def navigate(self, initPos, targetPos):
+        """
+        :param initPos: a 2 element list of the current position of the robot [x,y]
+        :param initPos: a 2 element list of the position that we want the robot to go to [x,y]
+        """
+        cost = 1
 
-    path = []
-    for i in range(len(revPath)):
-    	path.append(revPath[len(revPath) - 1 - i])
+        # has the cost of the robot going away from the goal mapped out so it will only check high cost 
+        # stuff if there are no other options
+        heuristic = [[0 for row in range(len(self.field[0]))] for col in range(len(self.field))]
+        for i in range(len(self.field)):    
+            for j in range(len(self.field[0])):            
+                heuristic[i][j] = abs(i - targetPos[0]) + abs(j - targetPos[1])
+                if self.field[i][j][0] != 255:
+                    heuristic[i][j] = 100 # if there is an obstacle in the spot on the field, adds a very high 
+                    # cost to going over that spot so it will be navigated around
 
-    return path
-
-def navigate():
-    return search(field, initPos, targetPos, cost, heuristic)
-
-print(navigate())
+        return self.search(self.field, initPos, targetPos, cost, heuristic)
