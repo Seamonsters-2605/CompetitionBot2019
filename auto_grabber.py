03ee105ff25eb53dff6e98e3725a80f9798609ec
Seamonsters <frc2605@gmail.com>
2019-02-19 20:53:50 -0800

    Use multiDrive for driveBackFromWall

--- auto_grabber.py
+++ auto_grabber.py
@@ -10,17 +10,16 @@ def driveWait(drive, time):
         drive.drive(0, 0, 0)
         yield
 
-def driveBackFromWall(drive):
-    drivetrain.autoPositionGear.applyGear(drive)
-    for _ in range(25): # one foot?
-        drive.drive(2, BACKWARD, 0)
+def driveBackFromWall(drive, superDrive):
+    drivetrain.autoPositionGear.applyGear(superDrive)
+    for _ in range(20): # something?
+        drive.drive(3, BACKWARD, 0)
         yield
 
 def pickUpHatch(drive, grabber, superDrive):        # PICK UP HATCH
     grabber.elevatorLifted()                        # elevator lift up
     yield from driveWait(drive, 25)                 # wait for elevator TODO
-    yield from driveBackFromWall(superDrive)        # drive backward
-    grabber.elevatorHatchPosition(1)                # elevator down
+    yield from driveBackFromWall(drive, superDrive) # drive backward
     grabber.setInnerPiston(False)                   # retract inner piston
     drive.drive(0, 0, 0)
     yield                                           # END
@@ -30,7 +29,7 @@ def pickUpCargo(drive, grabber, superDrive):        # PICK UP CARGO
     grabber.intake()                                # intake
     yield from driveWait(drive, 30)                 # wait for intake
     grabber.stopIntake()                            # stop intake
-    yield from driveBackFromWall(superDrive)        # drive backward
+    yield from driveBackFromWall(drive, superDrive) # drive backward
     drive.drive(0, 0, 0)
     yield                                           # END
 
@@ -39,7 +38,7 @@ def depositHatch(drive, grabber, pos, superDrive):  # DEPOSIT HATCH
     yield from driveWait(drive, 50)                 # wait for elevator TODO
     grabber.setOuterPiston(True)                    # extend outer pistons
     yield from driveWait(drive, 25)                 # wait for pistons
-    yield from driveBackFromWall(superDrive)        # drive backward
+    yield from driveBackFromWall(drive, superDrive) # drive backward
     grabber.elevatorHatchPosition(1)                # elevator down
     grabber.setOuterPiston(False)                   # retract outer pistons
     drive.drive(0, 0, 0)
@@ -51,7 +50,7 @@ def depositCargo(drive, grabber, pos, superDrive):  # DEPOSIT CARGO
     grabber.eject()                                 # eject cargo
     yield from driveWait(drive, 25)                 # wait for eject
     grabber.stopIntake()                            # stop intake
-    yield from driveBackFromWall(superDrive)        # drive backward
+    yield from driveBackFromWall(drive, superDrive) # drive backward
     grabber.elevatorCargoPosition(1)                # elevator down
     drive.drive(0, 0, 0)
     yield                                           # END
