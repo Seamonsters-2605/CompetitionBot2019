43f473ea5cc7a9b03de1b61e4b19e17ca362984d
Seamonsters <frc2605@gmail.com>
2019-03-15 17:10:27 -0700

    Log camera server errors

--- camera.py
+++ camera.py
@@ -1,14 +1,18 @@
 import cscore
 import time
+import logging
 
-print("imported camera!")
+logger = logging.getLogger("cameraserver")
 
 def main():
-    cam = cscore.UsbCamera("usbcam0", 0)
-    cam.setVideoMode(cscore.VideoMode.PixelFormat.kMJPEG, 320, 240, 30)
-    cam.setBrightness(40)
-    mjpegServer = cscore.MjpegServer(name="httpserver0", port=5806)
-    mjpegServer.setSource(cam)
-
-    while True:
-        time.sleep(0.1)
+    logger.info("The camera server started! you made it this far i'm proud of you")
+    try:
+        cam = cscore.UsbCamera("usbcam0", 0)
+        cam.setVideoMode(cscore.VideoMode.PixelFormat.kMJPEG, 320, 240, 30)
+        cam.setBrightness(40)
+        mjpegServer = cscore.MjpegServer(name="httpserver0", port=5806)
+        mjpegServer.setSource(cam)    
+        while True:
+            time.sleep(0.1)
+    except Exception as e:
+        print("Camera server is dead :(", e)
